c119d0d12d94c994b5bcb371e423d714dee1843e
Paulo Arthur <93049254+Reynald-V1@users.noreply.github.com>
2023-10-01 10:43:19 -0300

    Initial commit

---
+++ app/src/main/java/com/example/banco/CadastraActivity.kt
@@ -0,0 +1,24 @@
+package com.example.banco
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.banco.databinding.ActivityCadastraBinding
+import com.example.banco.databinding.ActivityMainBinding
+
+class CadastraActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCadastraBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_cadastra)
+
+        binding.buttonCancela.setOnClickListener{
+            var i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/banco/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.banco
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.banco.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.buttonCadastrar.setOnClickListener{
+            var i = Intent(this, CadastraActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
